cae90932eb757dfc60c15c81ccf48890241f4ef5
scliff6262 <sclifford6262@gmail.com>
2018-09-18 17:48:47 -0400

    resolved merge conflicts

--- pos-api/app/models/user.rb
+++ pos-api/app/models/user.rb
@@ -1,10 +1,3 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   has_secure_password
-=======
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
->>>>>>> 6da0e3c06ef1cae93427f61a1f11c47d1f6e89d1
 end
