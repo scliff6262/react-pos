996d17994aceb6600e3c583ef608c2d03d1f555c
scliff6262 <sclifford6262@gmail.com>
2018-09-24 23:54:57 -0400

    checks properly display price for each item, tax and total

--- client/src/components/Check.js
+++ client/src/components/Check.js
@@ -1,12 +1,15 @@
 import React from 'react'
 
 const Check = (props) => {
-  const items = props.initialItems.map( (item) => <li data-price={item.price}>{item.name}</li> )
-  const itemsToAdd = props.itemsToAdd.map( (item) => <li data-price={(item.price)} onClick={props.removeLastNewItem}>{item.name}</li> )
+  const items = props.initialItems.map( (item) => <li data-price={item.price}>{item.name} - ${item.price}</li> )
+  const itemsToAdd = props.itemsToAdd.map( (item) => <li data-price={(item.price)} onClick={props.removeLastNewItem}>{item.name} - ${item.price}</li> )
   let subtotal = 0
 
   props.initialItems.forEach( item => subtotal += parseInt(item.price) )
   props.itemsToAdd.forEach( item => subtotal += parseInt(item.price) )
+
+  const tax = Math.round(100 * (subtotal * .08875))/100
+  const total = tax + subtotal
 
   return (
     <div>
@@ -15,7 +18,9 @@ const Check = (props) => {
         {items}
         {itemsToAdd}
       </ul>
-      <p>Subtotal: {subtotal} </p>
+      <p>Subtotal: ${subtotal} </p>
+      <p>Tax: ${tax}</p>
+      <p>Total: $ {total}</p>
     </div>
   )
 }
